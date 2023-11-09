23bc3f192f9e3a03ba8f6cc44f3630060b85bf46
Ayodeji Balogun <manuelbalogun@gmail.com>
2023-11-09 11:42:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a Fact";
   }
 });
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+let votesInteresting = 20;
+let votesMindBlowing = 20;
+
+if (votesInteresting === votesMindBlowing) {
+  alert("This fact is equally interesting and mindblowing!");
+}
